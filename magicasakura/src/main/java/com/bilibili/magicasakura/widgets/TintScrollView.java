52152d3144cc4429e12b5d60550273db63d9ec41
djy <137359049@qq.com>
2018-01-21 12:11:18 +0800

    1.添加LinearLayout分割线换肤适配 2.添加ScrollView换肤适配

--- magicasakura/src/main/java/com/bilibili/magicasakura/widgets/TintScrollView.java
+++ magicasakura/src/main/java/com/bilibili/magicasakura/widgets/TintScrollView.java
@@ -1,52 +1,31 @@
-/*
- * Copyright (C) 2016 Bilibili
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.bilibili.magicasakura.widgets;
 
 import android.content.Context;
-import android.content.res.TypedArray;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
-import android.graphics.drawable.GradientDrawable;
-import android.os.Build;
 import android.util.AttributeSet;
-import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
-import com.bilibili.magicasakura.R;
-import com.bilibili.magicasakura.utils.ThemeUtils;
 import com.bilibili.magicasakura.utils.TintManager;
 
 /**
- * @author xyczero617@gmail.com
- * @time 16/2/14
+ * @author xia
+ * @date 2018/1/21.
  */
-@SuppressWarnings("deprecation")
-public class TintLinearLayout extends LinearLayout implements Tintable, AppCompatBackgroundHelper.BackgroundExtensible {
-    private AppCompatBackgroundHelper mBackgroundHelper;
-    private int mDividerColorId;
 
-    public TintLinearLayout(Context context) {
+@SuppressWarnings("deprecation")
+public class TintScrollView extends ScrollView implements Tintable, AppCompatBackgroundHelper.BackgroundExtensible {
+    private AppCompatBackgroundHelper mBackgroundHelper;
+
+    public TintScrollView(Context context) {
         this(context, null);
     }
 
-    public TintLinearLayout(Context context, AttributeSet attrs) {
-        this(context, attrs, 0);
+    public TintScrollView(Context context, AttributeSet attrs) {
+        this(context, attrs, android.R.attr.scrollViewStyle);
     }
 
-    public TintLinearLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+    public TintScrollView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         if (isInEditMode()) {
             return;
@@ -54,10 +33,6 @@ public class TintLinearLayout extends LinearLayout implements Tintable, AppCompa
         final TintManager tintManager = TintManager.get(context);
         mBackgroundHelper = new AppCompatBackgroundHelper(this, tintManager);
         mBackgroundHelper.loadFromAttribute(attrs, defStyleAttr);
-
-        final TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.TintLinearLayout);
-        mDividerColorId = ta.getResourceId(R.styleable.TintLinearLayout_dividerTint, 0);
-        ta.recycle();
     }
 
     @Override
@@ -104,20 +79,6 @@ public class TintLinearLayout extends LinearLayout implements Tintable, AppCompa
     public void setBackgroundTintList(int resId, PorterDuff.Mode mode) {
         if (mBackgroundHelper != null) {
             mBackgroundHelper.setBackgroundTintList(resId, mode);
-        }
-    }
-
-    @Override
-    public void setDividerDrawable(Drawable divider) {
-        super.setDividerDrawable(divider);
-        Drawable drawableDivider = null;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-            drawableDivider = getDividerDrawable();
-        }
-        if (drawableDivider != null && mDividerColorId != 0) {
-            final int color = ThemeUtils.getColorById(getContext(), mDividerColorId);
-            final GradientDrawable gradientDrawable = (GradientDrawable) drawableDivider;
-            gradientDrawable.setColor(color);
         }
     }
 
