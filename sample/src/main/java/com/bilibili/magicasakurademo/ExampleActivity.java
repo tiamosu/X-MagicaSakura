782019d9505500d8fa88c7f49247b19988f3de05
djy <137359049@qq.com>
2018-01-21 12:53:26 +0800

    1.添加示例测试

---
+++ sample/src/main/java/com/bilibili/magicasakurademo/ExampleActivity.java
@@ -0,0 +1,79 @@
+package com.bilibili.magicasakurademo;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.bilibili.magicasakura.utils.ThemeUtils;
+import com.bilibili.magicasakurademo.dialog.CardPickerDialog;
+import com.bilibili.magicasakurademo.utils.ThemeHelper;
+
+/**
+ * @author xia
+ * @date 2018/1/21.
+ */
+
+public class ExampleActivity extends AppCompatActivity implements CardPickerDialog.ClickListener {
+
+    @SuppressWarnings("ConstantConditions")
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_example);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(null);
+    }
+
+    @Override
+    public void onConfirm(int currentTheme) {
+        if (ThemeHelper.getTheme(ExampleActivity.this) != currentTheme) {
+            ThemeHelper.setTheme(ExampleActivity.this, currentTheme);
+            ThemeUtils.refreshUI(ExampleActivity.this, new ThemeUtils.ExtraRefreshable() {
+                        @Override
+                        public void refreshGlobal(Activity activity) {
+                            //for global setting, just do once
+                            if (Build.VERSION.SDK_INT >= 21) {
+                                final ExampleActivity context = ExampleActivity.this;
+                                ActivityManager.TaskDescription taskDescription =
+                                        new ActivityManager.TaskDescription(null, null,
+                                                ThemeUtils.getThemeAttrColor(context, android.R.attr.colorPrimary));
+                                setTaskDescription(taskDescription);
+                                getWindow().setStatusBarColor(
+                                        ThemeUtils.getColorById(context, R.color.theme_color_primary_dark));
+                            }
+                        }
+
+                        @Override
+                        public void refreshSpecificView(View view) {
+                        }
+                    }
+            );
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.change_theme) {
+            CardPickerDialog dialog = new CardPickerDialog();
+            dialog.setClickListener(this);
+            dialog.show(getSupportFragmentManager(), CardPickerDialog.TAG);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
