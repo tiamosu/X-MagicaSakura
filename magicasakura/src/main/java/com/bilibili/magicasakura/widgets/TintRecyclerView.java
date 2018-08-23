912307507dfd03e86ff41f9fea917b4a6720a895
djy <137359049@qq.com>
2018-08-23 11:18:40 +0800

    1.新增TintRecyclerView

---
+++ magicasakura/src/main/java/com/bilibili/magicasakura/widgets/TintRecyclerView.java
@@ -0,0 +1,92 @@
+package com.bilibili.magicasakura.widgets;
+
+import android.content.Context;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+
+import com.bilibili.magicasakura.utils.TintManager;
+
+/**
+ * @author xia
+ * @date 2018/8/23.
+ */
+public class TintRecyclerView extends RecyclerView implements Tintable,
+        AppCompatBackgroundHelper.BackgroundExtensible {
+
+    private AppCompatBackgroundHelper mBackgroundHelper;
+
+    public TintRecyclerView(Context context) {
+        this(context, null);
+    }
+
+    public TintRecyclerView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public TintRecyclerView(Context context, @Nullable AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        if (isInEditMode()) {
+            return;
+        }
+        final TintManager tintManager = TintManager.get(context);
+        mBackgroundHelper = new AppCompatBackgroundHelper(this, tintManager);
+        mBackgroundHelper.loadFromAttribute(attrs, defStyle);
+    }
+
+    @Override
+    protected void drawableStateChanged() {
+        super.drawableStateChanged();
+        if (getBackground() != null) {
+            invalidateDrawable(getBackground());
+        }
+    }
+
+    @Override
+    public void setBackground(Drawable background) {
+        super.setBackground(background);
+        if (mBackgroundHelper != null) {
+            mBackgroundHelper.setBackgroundDrawableExternal(background);
+        }
+    }
+
+    @Override
+    public void setBackgroundResource(int resId) {
+        if (mBackgroundHelper != null) {
+            mBackgroundHelper.setBackgroundResId(resId);
+        } else {
+            super.setBackgroundResource(resId);
+        }
+    }
+
+    @Override
+    public void setBackgroundColor(int color) {
+        super.setBackgroundColor(color);
+        if (mBackgroundHelper != null) {
+            mBackgroundHelper.setBackgroundColor(color);
+        }
+    }
+
+    @Override
+    public void setBackgroundTintList(int resId) {
+        if (mBackgroundHelper != null) {
+            mBackgroundHelper.setBackgroundTintList(resId, null);
+        }
+    }
+
+    @Override
+    public void setBackgroundTintList(int resId, PorterDuff.Mode mode) {
+        if (mBackgroundHelper != null) {
+            mBackgroundHelper.setBackgroundTintList(resId, mode);
+        }
+    }
+
+    @Override
+    public void tint() {
+        if (mBackgroundHelper != null) {
+            mBackgroundHelper.tint();
+        }
+    }
+}
